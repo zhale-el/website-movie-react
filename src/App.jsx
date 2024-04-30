43a39bc252b07e367874a1eb10f52980662bb0ec
zhaleh <zhalehelmi@gmail.com>
2024-04-30 16:04:54 +0330

    login style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 };
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 import "./Login.css";
+import Logo from "../../assets/images/logo.png";
+
 const Login = () => {
-  return <div className="login">Login page</div>;
+  return (
+    <div className="login">
+      <img src={Logo} alt="logo image" className="login__logo" />
+      <div className="login__form">
+        <h1 className="login__title">Sign Up</h1>
+        <form className="form">
+          <input type="text" className="form__input" placeholder="Your name" />
+          <input type="email" className="form__input" placeholder="Email" />
+          <input
+            type="password"
+            className="form__input"
+            placeholder="Password"
+          />
+          <button className="form__btn">Sign Up</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label>Remember Me</label>
+            </div>
+            <p>Need Help ?</p>
+          </div>
+        </form>
+
+        <div className="login-switch">
+          <p>
+            New to Netflix ? <span>Sign Up Now</span>
+          </p>
+
+          <p>
+            Already have account ? <span>Sign In Now</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
